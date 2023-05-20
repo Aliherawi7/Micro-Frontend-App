3ae1e93ebc5d3a59ef78ab7222864e89063aaa46
ali herawi <aliherawi7@gmail.com>
2023-05-20 06:42:55 +0430

    static notes added for test purpose

---
+++ projects/host-app/src/app/constants/Notes.ts
@@ -0,0 +1,40 @@
+export interface Note {
+    date: string,
+    title: string,
+    text: string,
+    color: string
+}
+
+
+export const notes: Note[] = [
+    {
+        date: 'Friday, May 19, 2023',
+        title: "Note title",
+        text: "this is for test",
+        color: 'orange'
+    },
+    {
+        date: 'Friday, May 19, 2023',
+        title: "Note title",
+        text: "this is for test",
+        color: 'green'
+    },
+    {
+        date: 'Friday, May 19, 2023',
+        title: "Note title",
+        text: "this is for test",
+        color: 'bule'
+    },
+    {
+        date: 'Friday, May 19, 2023',
+        title: "Note title",
+        text: "this is for test",
+        color: '#ddd'
+    },
+    {
+        date: 'Friday, May 19, 2023',
+        title: "Note title",
+        text: "this is for test",
+        color: 'red'
+    },
+]
